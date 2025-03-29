0850cacb69cbaab193939b904651de02bbace300
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-29 18:52:07 +0100

    Nav menu

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Home from "./booking/Home";
 import Login from "./auth/Login";
-import Register from "./auth/Register"
+import Register from "./auth/Register";
+import TopNav from "./components/TopNav";
+
+
+
+
 
 function App() {
   return (
     <BrowserRouter>
+      <TopNav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
