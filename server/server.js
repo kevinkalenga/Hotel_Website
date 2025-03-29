5d9bcfc40598566aee11fb934a81bed1bcd15df0
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-29 23:09:42 +0100

    Autoloading routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
-import express from "express"
+import express from "express";
+import { readdirSync } from "fs"
 
 const app = express();
 
+// route middleware 
+readdirSync("./routes").map((r) => app.use('/api', require(`./routes/${r}`)))
+
+
 const port = 8000
 
-app.get('/api/:message', (req, res) => {
-    res.status(200).send(req.params.message)
-});
+
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
