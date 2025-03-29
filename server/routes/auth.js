25394097b06559d6f79cffe0159199a9eb25421c
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-29 23:15:41 +0100

    Controllers

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,10 @@
 import express from "express";
+import { showMessage } from "../controller/auth"
 
 const router = express.Router()
 
 
 
-router.get('/:message', (req, res) => {
-    res.status(200).send(req.params.message)
-});
+router.get('/:message', showMessage);
 
 module.exports = router
