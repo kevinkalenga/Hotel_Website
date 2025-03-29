943f0ad069485fc9eadcf5e1911f0c40b02939ab
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-29 22:20:04 +0100

    Express server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const port = 8000
+
+app.get('/api/:message', (req, res) => {
+    res.status(200).send(req.params.message)
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
