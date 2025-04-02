8d2135ad2d3bd9a8484d9bc61c1b0f73a538450a
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-02 18:19:40 +0200

    User schema

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { showMessage, register } from "../controller/auth.js";
+import { showMessage, register } from "../controllers/auth.js";
 
 const router = express.Router()
 
