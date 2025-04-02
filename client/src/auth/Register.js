a19281b1e1b52a8f1305045a15c5b18e1b413c79
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-02 17:28:21 +0200

    Separate form component

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,50 +1,9 @@
-import { useState } from "react"
+import RegisterForm from "../components/RegisterForm"
+
 const Register = () => {
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log({ name, email, password })
-    }
-
-
     const registerForm = () => (
-        <form onSubmit={handleSubmit} className="mt-5">
-            <div className="form-group mb-3">
-                <label className="form-label">Your name</label>
-                <input type="name"
-                    className="form-control"
-                    placeholder="Enter name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-
-            </div>
-            <div className="form-group mb-3">
-                <label className="form-label">Email address</label>
-                <input type="email"
-                    className="form-control"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-
-            </div>
-            <div className="form-group mb-3">
-                <label className="form-label">Password</label>
-                <input type="password"
-                    className="form-control"
-                    placeholder="Enter password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-
-            </div>
-            <button className="btn btn-primary">Submit</button>
-        </form>
+        <RegisterForm />
     )
 
     return (
