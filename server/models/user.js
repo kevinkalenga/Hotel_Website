f7eee84113988ac9d9bd842d9d3ea7ad6f783cd2
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-07 14:58:13 +0200

    Compare password

--- server/models/user.js
+++ server/models/user.js
@@ -33,5 +33,19 @@ userSchema.pre('save', function (next) {
 
 // export default mongoose.model("User", userSchema);
 
+userSchema.methods.comparePassword = function (password, next) {
+    // this.password is the pwd that is coming from the database
+    bcrypt.compare(password, this.password, function (err, match) {
+        if (err) {
+            console.log("COMPARE PASSWORD ERR", err);
+            return next(err, false)
+        }
+
+        console.log("MATCH PASSWORD", match);
+        return next(null, match);
+    })
+}
+
 const User = mongoose.model("User", userSchema);
+
 export default User;
