32d95b72a2ab452b5c4ede73cc9ec729ad48e370
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-08 13:21:33 +0200

    Conditional rendering and logout

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,16 +1,49 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 const TopNav = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { auth } = useSelector((state) => ({ ...state }))
+
+    const logout = () => {
+        dispatch({
+            type: "LOGOUT",
+            payload: null
+        });
+        window.localStorage.removeItem('auth');
+        navigate('/login')
+    }
+
     return (
         <div className="nav bg-light d-flex justify-content-between boxshadow">
             <Link className="nav-link" to="/">
                 Home
             </Link>
-            <Link className="nav-link" to="/login">
-                Login
-            </Link>
-            <Link className="nav-link" to="/register">
-                Register
-            </Link>
+
+            {
+                auth !== null && (
+                    <Link onClick={logout} className="nav-link" to="/login">
+                        Logout
+                    </Link>
+                )
+            }
+
+
+            {
+                auth === null && (
+                    <>
+                        <Link className="nav-link" to="/login">
+                            Login
+                        </Link>
+                        <Link className="nav-link" to="/register">
+                            Register
+                        </Link>
+                    </>
+                )
+            }
+
+
+
         </div>
     )
 }
