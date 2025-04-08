ad1793666dfe6a97ea1a5cff92feb90a75be8dc0
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-08 12:51:30 +0200

    Redux state on page reload

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,5 +1,13 @@
+let userState;
+
+if (window.localStorage.getItem('auth')) {
+    userState = JSON.parse(window.localStorage.getItem("auth"));
+} else {
+    userState = null
+}
+
 // 2. create user reducer function ;
-export const authReducer = (state = {}, action) => {
+export const authReducer = (state = userState, action) => {
     switch (action.type) {
         case "LOGGED_IN_USER":
             return { ...state, ...action.payload };
